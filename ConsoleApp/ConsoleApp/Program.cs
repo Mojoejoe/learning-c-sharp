f691454b339c810462a74ec5be337bf403e1e4c0
jcvejic <j.cvejic@case-3d.com>
2018-12-21 16:56:23 +0100

    napredak u chapteru 3

--- ConsoleApp/ConsoleApp/Program.cs
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -7,12 +7,20 @@ using System.Threading.Tasks;
 namespace ConsoleApp {
   class Program {
     static void Main(string[] args) {
-      // The code provided will print ‘Hello World’ to the console.
-      // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
-      Console.WriteLine("Hello World!");
-      Console.ReadKey();
 
-      // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
+      GetUserData();
+      Console.ReadLine();     
+      
+    }
+
+    private static void GetUserData() {
+
+      Console.WriteLine("Write your name");
+      string name = Console.ReadLine();
+      Console.WriteLine("Write your age");
+      string age = Console.ReadLine();
+
+      Console.WriteLine("Hello {0} you are {1} years old!",name,age);
     }
   }
 }
